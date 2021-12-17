9fc63cef7a2890f57fa39869d9c000ed69186415
Matheus Sodré <33385453+MatheusSodreS@users.noreply.github.com>
2021-12-17 08:58:09 -0300

    Add files via upload

---
+++ app/src/main/java/br/com/dio/todolist/ui/MainActivity.kt
@@ -0,0 +1,61 @@
+package br.com.dio.todolist.ui
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import br.com.dio.todolist.databinding.ActivityMainBinding
+import br.com.dio.todolist.datasource.TaskDataSource
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val adapter by lazy { TaskListAdapter() }
+
+    /**
+     * Nova maneira de iniciar uma activity.
+     * Já que `startActivityForResult` foi depreciado.
+     */
+    private val register =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == Activity.RESULT_OK) updateList()
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.rvTasks.adapter = adapter
+        updateList()
+
+        insertListeners()
+    }
+
+    private fun insertListeners() {
+        binding.fab.setOnClickListener {
+            register.launch(Intent(this, AddTaskActivity::class.java))
+        }
+
+        adapter.listenerEdit = {
+            val intent = Intent(this, AddTaskActivity::class.java)
+            intent.putExtra(AddTaskActivity.TASK_ID, it.id)
+            register.launch(intent)
+        }
+
+        adapter.listenerDelete = {
+            TaskDataSource.deleteTask(it)
+            updateList()
+        }
+    }
+
+    private fun updateList() {
+        val list = TaskDataSource.getList()
+        binding.includeEmpty.emptyState.visibility = if (list.isEmpty()) View.VISIBLE
+        else View.GONE
+
+        adapter.submitList(list)
+    }
+}
